1d2b70928a4bde1d162874deea69dc540c010ade
Juan Carlos <juan@thirstie.com>
2018-04-12 11:14:27 -0300

    Remove dependency [SKIP CI]

--- css-html-prettify.py
+++ css-html-prettify.py
@@ -353,6 +353,30 @@ def html_prettify(html: str, extraline: bool=False) -> str:
 ##############################################################################
 
 
+def walk2list(folder: str, target: tuple, omit: tuple=(),
+              showhidden: bool=False, topdown: bool=True,
+              onerror: object=None, followlinks: bool=False) -> tuple:
+    """Perform full walk, gather full path of all files,
+    based on os.walk with multiple output types & extras.
+    Returns a namedtuple 'walk2list' with multiple output types:
+    - List represents folder/file structure of folder.
+    - JSON dumps string of the list, uses uJSON if installed.
+    - tuple of the list.
+    - set of the list.
+    - frozenset of the list.
+    - collections.deque of the list."""
+    oswalk = os.walk(folder, topdown=topdown,
+                     onerror=onerror, followlinks=followlinks)
+
+    lst = [os.path.abspath(os.path.join(r, f))
+           for r, d, fs in oswalk
+           for f in fs if not f.startswith(() if showhidden else ".") and
+           not f.endswith(omit) and
+           f.endswith(target)]
+
+    return tuple(lst)
+
+
 def process_multiple_files(file_path):
     """Process multiple CSS, HTML files with multiprocessing."""
     print(f"Process {os.getpid()} is processing {file_path}.")
